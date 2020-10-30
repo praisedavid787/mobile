25aaad458aa6be60d27d8892aff9f394c5bba002
David Akpan <praisedavid787@gmail.com>
2020-10-30 10:38:48 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// const form = document.forms[0];
+
+// const input = form.elements.searchInput;
+// input.addEventListener('focus', function() { 
+//     if (input.value==='Search Here') {
+//         input.value = '';
+//     } }, false
+// );
+// input.addEventListener('blur', function() {
+//     if(input.value === '') {
+//         input.value = 'Search Here';
+//     } }, false
+// );
+// input.addEventListener('change', () => console.log('changed'), false);
+
+// form.addEventListener ('submit', search, false);
+// function search(e) {
+// alert(`You searched for ${input.value}.`);
+// console.log('Form Submitted!');
+// event.preventDefault();
+// }
+
+// input.value = 'Search Here';
